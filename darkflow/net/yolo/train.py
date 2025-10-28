2e04a524158e00809bcc985d54bc1b0fc0d891e8
Surwase Vinay Dnyaneshwar <146312926+VinaySurwase@users.noreply.github.com>
2025-10-28 07:37:08 +0530

    Initial commit

---
+++ darkflow/net/yolo/train.py
@@ -0,0 +1,79 @@
+import tensorflow.contrib.slim as slim
+import pickle
+import tensorflow as tf
+from .misc import show
+import numpy as np
+import os
+
+def loss(self, net_out):
+
+    m = self.meta
+    sprob = float(m['class_scale'])
+    sconf = float(m['object_scale'])
+    snoob = float(m['noobject_scale'])
+    scoor = float(m['coord_scale'])
+    S, B, C = m['side'], m['num'], m['classes']
+    SS = S * S 
+
+    print('{} loss hyper-parameters:'.format(m['model']))
+    print('\tside    = {}'.format(m['side']))
+    print('\tbox     = {}'.format(m['num']))
+    print('\tclasses = {}'.format(m['classes']))
+    print('\tscales  = {}'.format([sprob, sconf, snoob, scoor]))
+
+    size1 = [None, SS, C]
+    size2 = [None, SS, B]
+
+    _probs = tf.placeholder(tf.float32, size1)
+    _confs = tf.placeholder(tf.float32, size2)
+    _coord = tf.placeholder(tf.float32, size2 + [4])
+    _proid = tf.placeholder(tf.float32, size1)
+    _areas = tf.placeholder(tf.float32, size2)
+    _upleft = tf.placeholder(tf.float32, size2 + [2])
+    _botright = tf.placeholder(tf.float32, size2 + [2])
+
+    self.placeholders = {
+        'probs':_probs, 'confs':_confs, 'coord':_coord, 'proid':_proid,
+        'areas':_areas, 'upleft':_upleft, 'botright':_botright
+    }
+
+    coords = net_out[:, SS * (C + B):]
+    coords = tf.reshape(coords, [-1, SS, B, 4])
+    wh = tf.pow(coords[:,:,:,2:4], 2) * S 
+    area_pred = wh[:,:,:,0] * wh[:,:,:,1] 
+    centers = coords[:,:,:,0:2] 
+    floor = centers - (wh * .5) 
+    ceil  = centers + (wh * .5) 
+
+    intersect_upleft   = tf.maximum(floor, _upleft)
+    intersect_botright = tf.minimum(ceil , _botright)
+    intersect_wh = intersect_botright - intersect_upleft
+    intersect_wh = tf.maximum(intersect_wh, 0.0)
+    intersect = tf.multiply(intersect_wh[:,:,:,0], intersect_wh[:,:,:,1])
+
+    iou = tf.truediv(intersect, _areas + area_pred - intersect)
+    best_box = tf.equal(iou, tf.reduce_max(iou, [2], True))
+    best_box = tf.to_float(best_box)
+    confs = tf.multiply(best_box, _confs)
+
+    conid = snoob * (1. - confs) + sconf * confs
+    weight_coo = tf.concat(4 * [tf.expand_dims(confs, -1)], 3)
+    cooid = scoor * weight_coo
+    proid = sprob * _proid
+
+    probs = slim.flatten(_probs)
+    proid = slim.flatten(proid)
+    confs = slim.flatten(confs)
+    conid = slim.flatten(conid)
+    coord = slim.flatten(_coord)
+    cooid = slim.flatten(cooid)
+
+    self.fetch += [probs, confs, conid, cooid, proid]
+    true = tf.concat([probs, confs, coord], 1)
+    wght = tf.concat([proid, conid, cooid], 1)
+    print('Building {} loss'.format(m['model']))
+    loss = tf.pow(net_out - true, 2)
+    loss = tf.multiply(loss, wght)
+    loss = tf.reduce_sum(loss, 1)
+    self.loss = .5 * tf.reduce_mean(loss)
+    tf.summary.scalar('{} loss'.format(m['model']), self.loss)
---
+++ darkflow/net/yolov2/train.py
@@ -0,0 +1,93 @@
+import tensorflow.contrib.slim as slim
+import pickle
+import tensorflow as tf
+from ..yolo.misc import show
+import numpy as np
+import os
+import math
+
+def expit_tensor(x):
+	return 1. / (1. + tf.exp(-x))
+
+def loss(self, net_out):
+    m = self.meta
+    sprob = float(m['class_scale'])
+    sconf = float(m['object_scale'])
+    snoob = float(m['noobject_scale'])
+    scoor = float(m['coord_scale'])
+    H, W, _ = m['out_size']
+    B, C = m['num'], m['classes']
+    HW = H * W 
+    anchors = m['anchors']
+
+    print('{} loss hyper-parameters:'.format(m['model']))
+    print('\tH       = {}'.format(H))
+    print('\tW       = {}'.format(W))
+    print('\tbox     = {}'.format(m['num']))
+    print('\tclasses = {}'.format(m['classes']))
+    print('\tscales  = {}'.format([sprob, sconf, snoob, scoor]))
+
+    size1 = [None, HW, B, C]
+    size2 = [None, HW, B]
+    _probs = tf.placeholder(tf.float32, size1)
+    _confs = tf.placeholder(tf.float32, size2)
+    _coord = tf.placeholder(tf.float32, size2 + [4])
+    _proid = tf.placeholder(tf.float32, size1)
+    _areas = tf.placeholder(tf.float32, size2)
+    _upleft = tf.placeholder(tf.float32, size2 + [2])
+    _botright = tf.placeholder(tf.float32, size2 + [2])
+
+    self.placeholders = {
+        'probs':_probs, 'confs':_confs, 'coord':_coord, 'proid':_proid,
+        'areas':_areas, 'upleft':_upleft, 'botright':_botright
+    }
+
+    net_out_reshape = tf.reshape(net_out, [-1, H, W, B, (4 + 1 + C)])
+    coords = net_out_reshape[:, :, :, :, :4]
+    coords = tf.reshape(coords, [-1, H*W, B, 4])
+    adjusted_coords_xy = expit_tensor(coords[:,:,:,0:2])
+    adjusted_coords_wh = tf.sqrt(tf.exp(coords[:,:,:,2:4]) * np.reshape(anchors, [1, 1, B, 2]) / np.reshape([W, H], [1, 1, 1, 2]))
+    coords = tf.concat([adjusted_coords_xy, adjusted_coords_wh], 3)
+
+    adjusted_c = expit_tensor(net_out_reshape[:, :, :, :, 4])
+    adjusted_c = tf.reshape(adjusted_c, [-1, H*W, B, 1])
+
+    adjusted_prob = tf.nn.softmax(net_out_reshape[:, :, :, :, 5:])
+    adjusted_prob = tf.reshape(adjusted_prob, [-1, H*W, B, C])
+
+    adjusted_net_out = tf.concat([adjusted_coords_xy, adjusted_coords_wh, adjusted_c, adjusted_prob], 3)
+
+    wh = tf.pow(coords[:,:,:,2:4], 2) * np.reshape([W, H], [1, 1, 1, 2])
+    area_pred = wh[:,:,:,0] * wh[:,:,:,1]
+    centers = coords[:,:,:,0:2]
+    floor = centers - (wh * .5)
+    ceil  = centers + (wh * .5)
+
+    intersect_upleft   = tf.maximum(floor, _upleft)
+    intersect_botright = tf.minimum(ceil , _botright)
+    intersect_wh = intersect_botright - intersect_upleft
+    intersect_wh = tf.maximum(intersect_wh, 0.0)
+    intersect = tf.multiply(intersect_wh[:,:,:,0], intersect_wh[:,:,:,1])
+
+    iou = tf.truediv(intersect, _areas + area_pred - intersect)
+    best_box = tf.equal(iou, tf.reduce_max(iou, [2], True))
+    best_box = tf.to_float(best_box)
+    confs = tf.multiply(best_box, _confs)
+
+    conid = snoob * (1. - confs) + sconf * confs
+    weight_coo = tf.concat(4 * [tf.expand_dims(confs, -1)], 3)
+    cooid = scoor * weight_coo
+    weight_pro = tf.concat(C * [tf.expand_dims(confs, -1)], 3)
+    proid = sprob * weight_pro
+
+    self.fetch += [_probs, confs, conid, cooid, proid]
+    true = tf.concat([_coord, tf.expand_dims(confs, 3), _probs ], 3)
+    wght = tf.concat([cooid, tf.expand_dims(conid, 3), proid ], 3)
+
+    print('Building {} loss'.format(m['model']))
+    loss = tf.pow(adjusted_net_out - true, 2)
+    loss = tf.multiply(loss, wght)
+    loss = tf.reshape(loss, [-1, H*W*B*(4 + 1 + C)])
+    loss = tf.reduce_sum(loss, 1)
+    self.loss = .5 * tf.reduce_mean(loss)
+    tf.summary.scalar('{} loss'.format(m['model']), self.loss)
